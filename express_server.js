3f05d5acaef3a0163c83ca6a5818c8e4c77cbf10
Kade Kobussen <kadekobussen50@gmail.com>
2023-03-21 11:09:48 -0700

    Add form to urls_show.ejs that submits an updated longURL Add Edit button to each object in urls_index.ejs which takes you to appropriate urls_show page Add POST route for /urls/:id to update a resource

--- express_server.js
+++ express_server.js
@@ -79,3 +79,9 @@ app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[id];
   res.redirect('/urls');
 })
+
+app.post("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const newLongURL = req.body.longURL;
+  res.redirect('/urls');
+})

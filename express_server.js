07fc1f6dcff64b43b7ff1fed2d1d6c6640cb3b2b
Kade Kobussen <kadekobussen50@gmail.com>
2023-03-24 14:54:11 -0700

    cleaned up and reorganised code, started setup of README.md

--- express_server.js
+++ express_server.js
@@ -6,6 +6,10 @@ const PORT = 8080; // default port 8080
 const cookieSession = require("cookie-session")
 
 app.set("view engine", "ejs");
+
+//sets up middleware to parse incoming request bodies containing URL encoded data.
+app.use(express.urlencoded({ extended: true }));
+
 //lets app use cookieSession
 app.use(
   cookieSession({
@@ -25,6 +29,21 @@ const urlDatabase = {
     userID: "aJ48lW",
   },
 }
+
+//user database
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
 // used to generate ids
 function generateRandomString() {
   return Math.random().toString(36).substring(2, 8);
@@ -49,23 +68,6 @@ function getUserByEmail(email) {
   }
   return null;
 }
-
-//user database
-const users = {
-  userRandomID: {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur",
-  },
-  user2RandomID: {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk",
-  },
-};
-
-//sets up middleware to parse incoming request bodies containing URL encoded data.
-app.use(express.urlencoded({ extended: true }));
 
 // asked a mentor about this route while debugging and was told it wasnt needed because urls is our home route??
 
@@ -107,6 +109,22 @@ app.get("/urls", (req, res) => {
   }
 });
 
+// creates a new short URL for the long URL specified in the request body
+app.post("/urls", (req, res) => {
+  const userID = req.session.user_id;
+  const longURL = req.body.longURL;
+  if (!userID) {
+    return res.status(401).send("Unauthorized");
+  }
+  const id = generateRandomString();
+  urlDatabase[id] = {
+    longURL: longURL,
+    userID: userID,
+  };
+  console.log("test", urlDatabase)
+  res.redirect(`/urls/${id}`);
+});
+
 //displays the page for creating a new URL
 app.get("/urls/new", (req, res) => {
   const userId = req.session.user_id;
@@ -145,21 +163,6 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-// creates a new short URL for the long URL specified in the request body
-app.post("/urls", (req, res) => {
-  const userID = req.session.user_id;
-  const longURL = req.body.longURL;
-  if (!userID) {
-    return res.status(401).send("Unauthorized");
-  }
-  const id = generateRandomString();
-  urlDatabase[id] = {
-    longURL: longURL,
-    userID: userID,
-  };
-  console.log("test", urlDatabase)
-  res.redirect(`/urls/${id}`);
-});
 
 //redirects the user to the long URL for the short URL specified in the request parameter
 app.get("/u/:id", (req, res) => {
@@ -227,43 +230,54 @@ app.post("/urls/:id", (req, res) => {
   const user = users[req.session.user_id];
   const id = req.params.id;
   const newLongURL = req.body.longURL;
-  
+
   if (!user) {
     res.status(401).send("Must be logged in to edit URLs.");
     return;
   }
-  
+
   if (!urlDatabase[id]) {
     res.status(404).send("The requested URL does not exist.");
     return;
   }
-  
+
   if (urlDatabase[id].userID !== user.id) {
     res.status(403).send("You do not have permission to edit this URL.");
     return;
   }
-  
+
   urlDatabase[id].longURL = newLongURL;
   res.redirect('/urls');
 });
 
-//authenticates the user with the email and password specified in the request body and starts a user session if successful
-app.post("/login", (req, res) => {
-  const { email, password } = req.body;
-  const user = getUserByEmail(email);
-  
-  if (!user) {
-    res.status(403).send("Invalid email address");
+//renders the login page if no user is currently logged in, and redirects to /urls if a user is already logged in.
+app.get("/login", (req, res) => {
+  const userId = req.session.user_id;
+  if (userId) {
+    res.redirect('/urls');
   } else {
-    const samePassword = bcrypt.compareSync(password, user.password);
-    if (!samePassword) {
-      res.status(403).send("Invalid password");
-    } else {
-      req.session.user_id = user.id
-      res.redirect("/urls");
-    }
+    res.render('login');
   }
-});
+
+  //authenticates the user with the email and password specified in the request body and starts a user session if successful
+  app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    const user = getUserByEmail(email);
+
+    if (!user) {
+      res.status(403).send("Invalid email address");
+    } else {
+      const samePassword = bcrypt.compareSync(password, user.password);
+      if (!samePassword) {
+        res.status(403).send("Invalid password");
+      } else {
+        req.session.user_id = user.id
+        res.redirect("/urls");
+      }
+    }
+  });
+
+})
 
 app.post("/logout", (req, res) => {
   req.session = null;
@@ -310,12 +324,3 @@ app.post("/registration", (req, res) => {
   res.redirect('/urls');
 });
 
-//renders the login page if no user is currently logged in, and redirects to /urls if a user is already logged in.
-app.get("/login", (req, res) => {
-  const userId = req.session.user_id;
-  if (userId) {
-    res.redirect('/urls');
-  } else {
-    res.render('login');
-  }
-})
